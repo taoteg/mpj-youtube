7e4beb5745be725be783e2fca45c3a96f04eb565
taoteg <taoteg@gmail.com>
2018-01-09 11:45:08 -0600

    Initial commit of video work.

---
+++ fp.hof.000.js
@@ -0,0 +1,161 @@
+
+underscore = require('./node_modules/underscore/underscore-min.js')
+
+console.log('\nMPJ "Fun Fun Function" YouTube Series:')
+console.log(' + Functional Javascript')
+console.log(' - Higher Order Functions')
+
+console.log('\nWhy use higher order functions?')
+console.log('\n- less bugs (easier to reason code)\n- better code reusability')
+
+console.log('\nTechnical:\n')
+console.log('functions are values.')
+console.log('They can be passed into other funcntiuons - higher order functions.')
+console.log('This facilitates composition.')
+
+console.log('\nExamples:\n')
+
+// Example vanilla function.
+function triple (x) {
+  return x * 3
+}
+
+// Anonymous function assigned to variable.
+var triple = function (x) {
+  return x * 3
+}
+
+// Taking it farther...
+var waffle = triple
+// console.log(waffle(30))   // 90
+
+// LIST TRANSFORMATIONS & HOF: Filter, (Reject), Find, Map, Reduce
+
+// Test Data.
+var animals = [
+ { name: 'Almotheuselah Slumbig', species: 'cat' },
+ { name: 'Bilgorbrino Callafey', species: 'dog' },
+ { name: 'Sneerdawb Glerfnerder', species: 'mouse' },
+ { name: 'Squeeshaw Megaplor', species: 'bird' },
+ { name: 'Zephnorslengdune Grelzborg', species: 'cat' },
+ { name: 'Xylophornbog Dildrongus', species: 'dog' },
+ { name: 'Waldo', species: 'fly' },
+ { name: 'Jurgencrudgeball Vinegrastor', species: 'bird' },
+ { name: 'Grimlorbahn Freshdig', species: 'cat' },
+ { name: 'Saquahatchee Tuxmond', species: 'dog' },
+ { name: 'Tryphtanus Mekidatees', species: 'mouse' },
+ { name: 'Vordungul Paradeegra', species: 'bird' },
+ { name: 'Nelfor Brythmeling', species: 'cat' },
+ { name: 'Draguleent Morgunalis', species: 'dog' },
+ { name: 'Fornitudinas Phaedridast', species: 'mouse' },
+ { name: 'Reglinarshal Mungdoon', species: 'bird' },
+ { name: 'Creptyfoil Smattershod', species: 'mouse' }
+]
+console.log('\nAnimals array: ', animals)
+
+
+// FILTER METHOD.
+
+// Using vanilla function.
+var dogs = []
+for (var i=0; i < animals.length; i++) {
+  if (animals[i].species === 'dog')
+    dogs.push(animals[i])
+}
+console.log('\nDogs array: \n', dogs)
+
+// Using filter function.
+var doges = animals.filter(function(animal) {
+  return animal.species === 'dog'
+})
+console.log('\nDoges array: \n', doges)
+
+// Breaking the filter function into parts.
+var isDog = function(animal) {
+  return animal.species === 'dog'
+}
+var modoges = animals.filter(isDog)
+console.log('\nModoges array: \n', modoges)
+
+// Try cats.
+var isCat = function(animal) {
+  return animal.species === 'cat'
+}
+var cats = animals.filter(isCat)
+console.log('\nCats array: \n', cats)
+
+
+// REJECT METHOD.
+
+// Try NOT dogs. Requires underscore.
+var notdoges = underscore.reject(animals, isDog)
+console.log('\nNotdoges array: \n', notdoges)
+
+
+// MAP METHOD.
+// GOAL: Get list of the names of all the animals.
+
+// Vanilla approach.
+var names = []
+for (var i=0; i < animals.length; i++) {
+  names.push(animals[i].name)
+}
+console.log('\nNames array: \n', names)
+
+// Using map function.
+var mapnames = animals.map(function(animal) {
+  return animal.name
+})
+console.log('\nMapnames array: \n', mapnames)
+
+// Return a new object instead.
+var momapnames = animals.map(function(animal) {
+  return animal.name + ' is a ' + animal.species
+})
+console.log('\nMomapnames array: \n', momapnames)
+
+// Arrow functions! Even more concise.
+// var arrownames = animals.map((animal) => { return animal.name })
+// var arrownames = animals.map((animal) => animal.name )
+var arrownames = animals.map((x) => x.name )
+console.log('\nArrownames array: \n', arrownames)
+
+
+// REDUCE METHOD.
+
+// Review:
+// MAP - Takes an array and transforms to array of same length with each individual item transformed.
+// FILTER - Takes an array and transforms it into a smaller (filtered) array.
+// REJECT - Same as filter but inverted.
+// FIND - Same as filter but only returns the first item.
+
+// So what is reduce? What if you can't find an object that fits the criteria for the other list transformations?
+// REDUCE can be used to implement any other list transformation. It is the SUPER list transformation method. It can express any list transformation.
+
+// Some Dummy Data.
+var orders = [
+  { amount: 250 },
+  { amount: 400 },
+  { amount: 100 },
+  { amount: 325 }
+]
+
+// GOAL: Summarize the amounts.
+
+// Vanilla method.
+var totalAmount = 0
+for (var i=0; i < orders.length; i++) {
+  totalAmount += orders[i].amount
+  console.log('\niteration: ', i, totalAmount)
+}
+console.log('\ntotalAmount: \n', totalAmount)
+
+// Using reduce.
+// var reduceTotalAmount = orders.reduce(function(sum, order) {
+//   console.log('\nreduce iteration: ', sum, order)
+//   return sum + order.amount
+// }, 0)
+
+// Arrow functions!
+var reduceTotalAmount = orders.reduce((sum, order) => sum + order.amount, 0)
+console.log('\nreduceTotalAmount: \n', reduceTotalAmount)
